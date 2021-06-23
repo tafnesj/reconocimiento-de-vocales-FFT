cd65a6397fe0c6e148990cc3f2dab1f7c4b9d9c3
Tafnes Jiménez <55866934+tafnesj@users.noreply.github.com>
2021-06-23 17:33:52 -0500

    Add comentarios :100:

--- GUI.py
+++ GUI.py
@@ -2,8 +2,6 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from functools import partial
-# from matplotlib.backends.back_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-# from matplotlib.figure import Figure
 import numpy as np
 import pyaudio
 import wave
@@ -13,22 +11,31 @@ import matplotlib.pyplot as plt
 import math
 import cmath
 
+# Función: Revertir la posición de los bloques
 def reverseBits(x,h):
     rev = '{:0{h}b}'.format(x, h=h)
     return(int(rev[::-1], 2))
-    
+
+# Función: Implementación de la FFT  
 def fft(signal):
     signal = list(np.copy(signal).astype(complex))
     # Iteramos por todas las capas de abajo hacia arriba
     n = len(signal)
+    # Altura en potencias de 2
     h = int(math.log2(n))
     for x in range(0,n):
+        # Llamado a la función de revertir
         rev_pos = reverseBits(x,h)
         if(rev_pos<x):
+            # Swap
             signal[rev_pos], signal[x] = signal[x],signal[rev_pos]
+    # Vamos por todas las capas
     for i in range(h-1,-1,-1):
+        # Tamaño del bloque
         sz = 1<<(h-i);
+        # Número de bloques
         blocks = 1<<i;
+        # Cálculo de las omegas [precalculado]
         w = [cmath.exp(-2j * cmath.pi * k / sz) for k in range(sz//2)]
         # Procesamiento de cada bloque
         for j in range(0,blocks):
@@ -37,14 +44,16 @@ def fft(signal):
             for k in range(0, sz//2):
                 u = signal[start+k]
                 v = signal[start+k+(sz//2)] * w[k]
+                # Pares
                 signal[start+k] = u+v
+                # Impares
                 signal[start+k+(sz//2)] = u-v
     return np.array(signal)
 
-
+# Función: Lectura de archivo de audio [muestras en el tiempo]
 def wave_file(file):
     global RATE
-    global data
+    # Leer 2^16 muestras
     FRAMES = 2**17
     wf = wave.open(file, "rb")
     RATE = wf.getframerate()
@@ -52,6 +61,7 @@ def wave_file(file):
     wf.close()
     return data
 
+# Función:: Graficar las señales
 def graficar(data_x, data_y, legends, label_x, label_y, title, xlim = None):
     for i in range(0, len(data_x)):
         plt.plot(data_x[i], data_y[i], label = legends[i], marker = "o")
@@ -65,56 +75,70 @@ def graficar(data_x, data_y, legends, label_x, label_y, title, xlim = None):
     plt.show()
     plt.clf()
 
+# Función: Extraer las frecuencias fundamentales
 def getFundamentalFrequencies(data, plot=True):
+    # Muestras hexadecimal a entero de 16 bits
     data = list(struct.unpack(str(len(data)//2) + 'h', data))
     SZ = 1
+    # Potencia de 2 más cercana
     while SZ < len(data):
         SZ<<=1
+    # Rellenamos de ceros
     while len(data) < SZ:
         data.append(0)
-        
-    frequencies = np.abs(fft(data))
+    
+    # Hallar la DFT del audio
+    frequencies = np.abs(fft(data)) # Tomamos sólo la magnitud
+    # Usar la mitad de las frecuencias [izquierda]
     frequencies = frequencies[:len(data)//2]
     if plot:
         eje_t = np.arange(0, len(data)) / RATE
+        # Gráfica: Tiempo
         graficar([eje_t], [data], ["Audio"], "Tiempo (s)", "Amplitud", "Entrada de audio")
         eje_f = np.arange(0, len(data)//2) / (len(data)/RATE)
+        # Gráfica: Frecuencia
         graficar([eje_f], [frequencies], ["Audio"], "Frecuencia (Hz)", "Amplitud", "Entrada de audio", (0,4000))
     
+    # Ajustar las frecuencias de acuerdo cn el espacio de muestreo
     freqs = [(frequencies[i],i / (len(data)/RATE)) for i in range(len(data)//2)]
+    # Ordenar de mayor a menor
     freqs.sort(reverse=True)
-
+    
+    # Tomamos los 3 máximos
     freq_funda = [freqs[0][1]]
+    # Iteramos por el arreglo de las frecuencias
     for i in range(1,len(freqs)):
         is_valid = True
         for j in freq_funda:
+            # Tomamos la frecuencia siguiente qye cumpla el parámetro
             if(abs(j-freqs[i][1])<100):
                 is_valid = False
         if(is_valid):
             freq_funda.append(freqs[i][1])
             if(len(freq_funda)==3):
                 break
+    # Si no existen 3 diferentes, colocamos un 0
     while(len(freq_funda)<3):
         freq_funda.append(0)
 
+    # Ordenamos ascendentemente
     freq_funda.sort()
     return freq_funda
 
-
+# Función: Frecuencia promedio
 def train():
     freq_av_h = {'a':[0,0,0],'e':[0,0,0],'i':[0,0,0],'o':[0,0,0],'u':[0,0,0]}
     num_hombres = 1
     for h in range(1,num_hombres+1):
         for v in ['a','e','i','o','u']:
             freq_funda = getFundamentalFrequencies(wave_file(F"hombre_0{h}/{v}.wav"),False)
-
+            # Promedio
             for x in range(3):
                 freq_av_h[v][x]+=freq_funda[x]/num_hombres
-
     return freq_av_h
 
 # Frecuencias mujeres
-freq_mujeres = {'a':[903,1129,2031],'e':[430,528,2772],'i':[240,480,2897],'o':[421,634,846],'u':[271,584,825]}
+freq_mujeres = {'a':[903,1129,2031],'e':[430,538,2772],'i':[240,480,2897],'o':[421,634,846],'u':[271,584,825]}
 # Frecuencias hombres
 freq_hombres = train()
 
@@ -136,19 +160,20 @@ def microphone():
 
 def play(data, channels, RATE):
     p = pyaudio.PyAudio()
-    play = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True)
+    play = p.open(format=pyaudio.paInt16, channels=channels, rate=RATE, output=True)
     play.write(data)
     play.stop_stream()
     play.close()
     p.terminate()
 
-
+# Identificar el patrón con la vocal más cercana
 def getNearestVowel(freq_funda, es_hombre=False):
     min_dif = 1e9
     vowel = 'a'
     # Frecuencias en hombres
     if(es_hombre==True):
         for i,j in freq_hombres.items():
+            # Distancia Manhattan del patrón actual con las frecuencias
             freq_dif = abs(freq_funda[0]-j[0])+abs(freq_funda[1]-j[1])+abs(freq_funda[2]-j[2])
             if(min_dif>freq_dif):
                 min_dif = freq_dif
@@ -156,6 +181,7 @@ def getNearestVowel(freq_funda, es_hombre=False):
     # Frecuencias en mujeres
     else:
         for i,j in freq_mujeres.items():
+            # Distancia Manhattan del patrón actual con las frecuencias
             freq_dif = abs(freq_funda[0]-j[0])+abs(freq_funda[1]-j[1])+abs(freq_funda[2]-j[2])
             if(min_dif>freq_dif):
                 min_dif = freq_dif
@@ -166,34 +192,34 @@ def getNearestVowel(freq_funda, es_hombre=False):
 def restore():
     return option.get()    
     
-
+# Interfaz: Elegir archivo preguardado
 def Select_file():
     global filename
     filename = filedialog.askopenfilename(initialdir = "./", title = "Selecciona un achivo")
     input_lbl.config(text=filename)
 
-
+# Interfaz: Iniciar programa
 def Run():
     gender = False
     if restore() == "Masculino":
         gender = True  
-        
+    # Resultado de la vocal
     result = getNearestVowel(getFundamentalFrequencies(wave_file(filename)), gender)
     result_lbl.configure(text="La vocal es: "+result)
 
-
+# Interfaz: Reproducir archivo cargado
 def Play_record():
     channels = 2
-    RATE = 44100
-    play(data, channels, RATE)
+    play(wave_file(filename), channels, RATE)
     
-
+# Interfaz: Grabar audio desde el micrófono
 def Record():
     gender = False
     if restore() == "Masculino":
         gender = True  
-    microphone_funda = getFundamentalFrequencies(microphone(), True)   
+    microphone_funda = getFundamentalFrequencies(microphone(), True)
     result = getNearestVowel(microphone_funda,gender)
+    # Resultado de la vocal
     result_lbl.configure(text="La vocal es: "+result)
 
 if __name__ == '__main__':
@@ -204,6 +230,7 @@ if __name__ == '__main__':
     input_frame = Frame(window)
     input_frame.pack()
     
+    # Botones de interfaz
     input_lbl = Label(input_frame, text="Ingresa un audio:")
     search_btn = Button(input_frame, text="Buscar", command=Select_file)
     record_btn = Button(input_frame, text="Grabar", command=Record)
